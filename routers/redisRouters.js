8412d3da9b145708f94588de1c5249cbc0500535
trq1 <trq1.lee@gmail.com>
2020-02-12 11:19:21 +0900

    hmset을 post로 호출 하게 하라
    * hmset을 ngrinder에서 호출 할 수 있도록 controller를 작성 하였습니다.

--- routers/redisRouters.js
+++ routers/redisRouters.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from '../routes';
-import { home, getRedis, setRedis } from '../controller/redisController';
+import { home, getRedis, setRedis, hSetRedis } from '../controller/redisController';
 
 const redisRouter = express.Router();
 
 redisRouter.get(routes.home, home);
 redisRouter.post(routes.getredis, getRedis);
 redisRouter.post(routes.setredis, setRedis);
+redisRouter.post(routes.setredis, setRedis);
+redisRouter.post(routes.hmsetredis, hSetRedis);
 
 export default redisRouter;
