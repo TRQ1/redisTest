125ccb188af6189ae1e73f1258cf6fe5e6e30e8a
Q1 <trq1.lee@gmail.com>
2020-02-12 11:25:27 +0900

    hmset을 post로 호출 하게 하라 (#8)
    * hmset을 ngrinder에서 호출 할 수 있도록 controller를 작성 하였습니다.

--- controller/redisController.js
+++ controller/redisController.js
@@ -1,4 +1,7 @@
-import { getFun, setFun } from '../tools/redis';
+import { getFun, hSetFun, hmSetFun } from '../tools/redis';
+import { jsonfile } from '../tools/json_parser';
+import { rendomInit } from '../tools/randomUtil';
+
 
 export const home = async (req, res) => {
     res.json({result: 'home'});
@@ -21,9 +24,32 @@ export const getRedis = async(req, res) => {
 
 export const setRedis = async(req, res) => {
     try {
-        return res.json({result: "success1"});
+        const callNumber = req.params.call;
+        console.log(callNumber)
+        for (var i = 0; i < callNumber; i++) {
+            const value = jsonfile[i];
+            const items = "items:" + [i]
+            await hSetFun(items, JSON.stringify(value));
+        }
+        return res.json({result: 'success1'});
     } catch(e) {
-        return res.json({result: "fail", message: e.message});
+        return res.json({result: 'fail', message: e.message});
+    }
+};
+
+export const hSetRedis = async(req, res) => {
+    try {
+        const callNumber = req.params.call;
+        console.log(callNumber)
+        for (var i = 0; i < callNumber; i++) {
+            const value = jsonfile[i];
+            const items = "items:" + [i]
+            await hmSetFun(items, 'product', jsonfile[i].product, 'price', jsonfile[i].price, 'location',
+                jsonfile[i].location, 'company', jsonfile[i].company);
+        }
+        return res.json({result: 'success1'});
+    } catch(e) {
+        return res.json({result: 'fail', message: e.message});
     }
 };
 
--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 const HOME = '/';
 const GET_REDIS = '/getredis/:key/:call';
-const SET_REDIS = '/setredis';
+const SET_REDIS = '/setredis/:call';
+const HMSET_REDIS = '/hmsetredis/:call';
 
 const routes = {
     home: HOME,
     getredis: GET_REDIS,
-    setredis: SET_REDIS
+    setredis: SET_REDIS,
+    hmsetredis: HMSET_REDIS
 }
 
 export default routes;
